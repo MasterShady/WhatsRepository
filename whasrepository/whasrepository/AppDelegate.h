335f5644b05b616614b4d6a85612add4aa4b9f25
shadylew <379587878@qq.com>
2016-01-13 15:19:36 +0800

    添加工程

---
+++ whasrepository/whasrepository/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  whasrepository
+//
+//  Created by lanou on 16/1/13.
+//  Copyright © 2016年 Shady Lew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ whasrepository/whasrepository/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  whasrepository
+//
+//  Created by lanou on 16/1/13.
+//  Copyright © 2016年 Shady Lew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
